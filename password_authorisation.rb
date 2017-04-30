fc604dbba58afd66ac10fccc356a25726614b1a0
Logan Nguyen <l0ng@live.com>
2017-04-30 09:25:12 +0000

    last commit before checking homework

---
+++ password_authorisation.rb
@@ -0,0 +1,14 @@
+def valid_number?(number_string)
+  number_string.to_i.to_s == number_string
+end
+
+puts 'please input 1st number'
+gets.chomp.to_i = a
+
+puts 'please input 1st number'
+gets.chomp.to_i = a
+
+if valid_number?(a) && valid_number?(b)
+  output = a / b
+  puts output
+end
